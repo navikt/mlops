ec0b89d64d847e8a90c4341505cb7a17e23756b1
d142796 <millad.dagdoni@nav.no>
2024-06-18 23:28:32 +0200

    load model from call + torch

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel, PeftConfig
+import torch
 import pandas as pd
 
-model = False
-tokenizer = False
 
 app = Flask(__name__)
 
@@ -12,25 +11,24 @@ app = Flask(__name__)
 def index():
     return "Velkommen! legg til følgende for å spørre meg /analyser/(din query)"
 
-def setupModel(model = False, tokenizer = False):
-    if(not model or not tokenizer):
-        base_model_path = 'RuterNorway/Llama-2-7b-chat-norwegian'
-        lora_path = './fine_tuned_lora'
-
-        base_model = AutoModelForCausalLM.from_pretrained(base_model_path).to("cpu")
-        peft_config = PeftConfig.from_pretrained(lora_path)
-        model = PeftModel.from_pretrained(base_model, lora_path).to("cpu")
-        model = model.merge_and_unload()
-        tokenizer = AutoTokenizer.from_pretrained(lora_path)
-
-        # Set the pad_token to be the same as the eos_token
-        tokenizer.pad_token = tokenizer.eos_token
-        model.config.use_cache = False
 
 @app.route("/analyser/<tekst>")
 def analyserTekst(tekst):
     print("INIT MODEL")
-    setupModel(model, tokenizer)
+
+    base_model_path = 'RuterNorway/Llama-2-7b-chat-norwegian'
+    lora_path = './fine_tuned_lora'
+
+    base_model = AutoModelForCausalLM.from_pretrained(base_model_path).to("cpu")
+    peft_config = PeftConfig.from_pretrained(lora_path)
+    model = PeftModel.from_pretrained(base_model, lora_path).to("cpu")
+    model = model.merge_and_unload()
+    tokenizer = AutoTokenizer.from_pretrained(lora_path)
+
+    # Set the pad_token to be the same as the eos_token
+    tokenizer.pad_token = tokenizer.eos_token
+    model.config.use_cache = False
+    print("DONE INIT")
     print("SKAL ANALYSERE TEKST " + str(tekst))
     inputs = tokenizer(tekst, return_tensors="pt")
     # Generate text
