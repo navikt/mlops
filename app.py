54e11b4635cb87b455f9e8e8dd343a3b7937b870
d142796 <millad.dagdoni@nav.no>
2024-06-25 22:41:51 +0200

    moved model init on call and not init

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ from peft import PeftModel, PeftConfig
 import torch
 import pandas as pd
 
-
-
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -19,22 +16,7 @@ def index():
 def health():
     return {"health":"OK"}, 200
 
-
-print("INIT MODEL")
-base_model_path = 'RuterNorway/Llama-2-7b-chat-norwegian'
-lora_path = './fine_tuned_lora'
-base_model = AutoModelForCausalLM.from_pretrained(base_model_path).to("cpu")
-#peft_config = PeftConfig.from_pretrained(lora_path)
-model = PeftModel.from_pretrained(base_model, lora_path).to("cpu")
-model = model.merge_and_unload()
-tokenizer = AutoTokenizer.from_pretrained(lora_path)
-
-# Set the pad_token to be the same as the eos_token
-tokenizer.pad_token = tokenizer.eos_token
-model.config.use_cache = False
-print("DONE DOWNLOADING MODEL!!!")
-
-
+# {"instances":[{"text":"hva heter Norges hovedstad?"}]}
 @app.route("/predict", methods=["POST"])
 def predict():
   # Get the JSON data from the request body
@@ -54,6 +36,19 @@ def predict():
   # Get only the first instance
   first_instance = instances[0]
   input_text = first_instance.get("text")
+  print("INIT MODEL")
+  base_model_path = 'RuterNorway/Llama-2-7b-chat-norwegian'
+  lora_path = './fine_tuned_lora'
+  base_model = AutoModelForCausalLM.from_pretrained(base_model_path).to("cpu")
+  peft_config = PeftConfig.from_pretrained(lora_path)
+  model = PeftModel.from_pretrained(base_model, lora_path).to("cpu")
+  model = model.merge_and_unload()
+  tokenizer = AutoTokenizer.from_pretrained(lora_path)
+
+  # Set the pad_token to be the same as the eos_token
+  tokenizer.pad_token = tokenizer.eos_token
+  model.config.use_cache = False
+  print("DONE DOWNLOADING MODEL!!!")
     
   print("SKAL ANALYSERE TEKST " + str(input_text))    
   inputs = tokenizer(input_text, return_tensors="pt")
@@ -66,19 +61,6 @@ def predict():
     print("FERDIG...")
     return jsonify({"generated_text": generated_text})  
   return jsonify({"generated_text": generated_text})
-
-@app.route("/analyser/<tekst>")
-def analyserTekst(tekst):
-    print("SKAL ANALYSERE TEKST " + str(tekst))
-    inputs = tokenizer(tekst, return_tensors="pt")
-    # Generate text
-    with torch.no_grad():
-        print("JOBBER")
-        outputs = model.generate(inputs.input_ids,max_length=300)
-        print("DECODER TEKST")
-        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        print("FERDIG...")
-    return generated_text
-
+ 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8080")
