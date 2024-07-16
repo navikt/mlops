e3f5c579086ce65d4e4825b62684d3fd142b04b1
d142796 <millad.dagdoni@nav.no>
2024-07-16 22:14:31 +0200

    lagt til cloud function jobb som starter pipeline når  noen laster en ny treningsfil

---
+++ JobCloudFunctionToStartPipelineOnFileUpload.py
@@ -0,0 +1,51 @@
+#functions-framework==3.*
+#google-api-python-client>=1.7.8,<2
+#google-cloud-aiplatform
+#PyYAML
+
+import functions_framework
+import base64
+import json
+from google.cloud import aiplatform
+
+PROJECT_ID = '368669329223'                  
+REGION = 'europe-west4'                            
+PIPELINE_ROOT = 'gs://test-repo/mlops7' 
+
+#https://cloud.google.com/vertex-ai/docs/pipelines/trigger-pubsub
+# Triggered by a change in a storage bucket
+@functions_framework.cloud_event
+def startTreningAvTryggtekst(cloud_event):
+    data = cloud_event.data
+
+    event_id = cloud_event["id"]
+    event_type = cloud_event["type"]
+
+    #bucket = data["bucket"]
+    #name = data["name"]
+    #metageneration = data["metageneration"]
+    #timeCreated = data["timeCreated"]
+    #updated = data["updated"]
+
+    #print(f"Event ID: {event_id}")
+    #print(f"Event type: {event_type}")
+    #print(f"Bucket: {bucket}")
+    #print(f"File: {name}")
+    #print(f"Metageneration: {metageneration}")
+    #print(f"Created: {timeCreated}")
+    #print(f"Updated: {updated}")
+    # Create a PipelineJob using the compiled pipeline from pipeline_spec_uri
+    aiplatform.init(
+        project=PROJECT_ID,
+        location=REGION,
+    )
+    job = aiplatform.PipelineJob(
+        display_name='startTreningAvTryggtekst',
+        template_path="gs://tiltak-mlops/pipeline-template/mlops_pipeline.json",
+        #pipeline_root=PIPELINE_ROOT,
+        enable_caching=False
+        #parameter_values=parameter_values
+    )
+
+    # Submit the PipelineJob
+    job.submit()
